2ba5997cd100128bcb5f2f0eeafe037d1ebe8dd8
bilal.alnaani@fh-erfurt.de <bilal0710>
2020-04-30 18:30:03 +0200

    Initial commit

---
+++ src/Main/java/de/cf/Adress.java
@@ -0,0 +1,11 @@
+package de.cf;
+
+public class Adress {
+    public static void main(String[] args){
+        System.out.println("Hello World2");
+        System.out.println("HUHU");
+        System.out.println("Bilal");
+
+
+    }
+}
---
+++ src/Test/java/de/cf/AdressTest.java
@@ -0,0 +1,4 @@
+package de.cf;
+
+public class AdressTest {
+}
